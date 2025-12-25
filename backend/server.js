bda02379baec43a68d3d35dcaaa432e234794867
Rinidh <rinidhdhokia@gmail.com>
2025-12-25 16:18:23 +0300

    feat: set up MongoDB connection and mongodb Nodejs driver to dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+connectDB();
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
