57fb5fddebfbb907e36670bf32ed801979dbc3ee
Rinidh <rinidhdhokia@gmail.com>
2025-12-25 17:26:50 +0300

    feat: implement product creation endpoint with validation, error handling and save to database; add mongoose type definitions

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,36 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-connectDB();
+app.use(express.json()); // Middleware to parse JSON bodies in req.body
 
+app.post("/products", async (req, res) => {
+  const fields = req.body;
+
+  if (!fields.name || !fields.price || !fields.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  const { name, price, image } = fields;
+
+  try {
+    const newProduct = new Product({ name, price, image });
+    const savedProduct = await newProduct.save();
+    res.status(201).json({ success: true, data: savedProduct });
+  } catch (error) {
+    console.log("Error creating product:", error);
+    res.status(500).json({ success: false, message: "Internal server Error" });
+  }
+});
+
+connectDB();
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
