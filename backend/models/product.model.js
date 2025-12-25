172791ac025e10b04679d74d01c737b1f40c6890
Rinidh <rinidhdhokia@gmail.com>
2025-12-25 16:49:43 +0300

    feat: create product collections' model with schema definition; include fields: name, price, and image

---
+++ backend/models/product.model.js
@@ -0,0 +1,23 @@
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // MongoDB automatically manage createdAt and updatedAt fields
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
