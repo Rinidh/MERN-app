44628e0cc63246a47bc743ed82f71369b5ee5fe3
Rinidh <rinidhdhokia@gmail.com>
2025-12-25 21:54:03 +0300

    feat: add GET endpoint for fetching all products and its error handling

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,16 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies in req.body
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error fetching products:", error);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const fields = req.body;
 
