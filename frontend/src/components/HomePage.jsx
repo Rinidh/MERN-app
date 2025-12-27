d48c961e6162991dee787363baae54b7bcac4721
Rinidh <rinidhdhokia@gmail.com>
2025-12-27 17:00:32 +0300

    feat: add content to HomePage with fallback message and link to create product when no products to display

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,5 +1,36 @@
 import React from "react";
+import { Container, Heading, Text, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW={"container.xl"} p={10}>
+      <VStack>
+        <Heading
+          as={"h1"}
+          fontSize={"xl"}
+          fontWeight={"bold"}
+          pb={4}
+          bgGradient={"linear-gradient(to-l, #00498eff, #0075ceff)"}
+          bgClip={"text"}
+        >
+          Available Products 🚀
+        </Heading>
+
+        {/* FALL BACK TEXT & LINK WHEN NO PRODUCTS FOUND */}
+        <Text fontWeight={"bold"} fontSize={"lg"} color={"gray.500"}>
+          No Products found 🥲{" "}
+          <Link to={"/create"}>
+            <Text
+              as={"span"}
+              color={"#0075ceff"}
+              _hover={{ textDecoration: "underline" }}
+            >
+              Create a Product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
 };
