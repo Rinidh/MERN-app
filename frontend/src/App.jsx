cd5c3aee855003c8c8936ded67d5f4a4693cfad8
Rinidh <rinidhdhokia@gmail.com>
2025-12-27 10:28:33 +0300

    feat: initialize frontend with React, Chakra UI, and Vite
    - Used package.json with dependencies' versions as at the tutor's time. See tutor's package.json: https://github.com/burakorkmez/mern-crash-course/blob/master/frontend/package.json
    - Set up main entry point (main.jsx) with ChakraProvider
    - Add a title and favicon to index.html

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Button>Button for demo</Button>
+    </>
+  );
+}
+
+export default App;
