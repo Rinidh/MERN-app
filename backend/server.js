a90cdac1c8f748f18b61bd94263699bf9f865ef6
Rinidh <rinidhdhokia@gmail.com>
2025-12-27 09:12:21 +0300

    refactor: use environment variable for server port configuration

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -11,6 +12,6 @@ app.use(express.json()); // Middleware to parse JSON bodies in req.body
 app.use("/api/products", productRoutes);
 
 connectDB();
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server is running on port:" + PORT);
 });
