436f9582225501f2891cf5b7d785178e469f23ad
Rinidh <rinidhdhokia@gmail.com>
2025-12-28 21:58:56 +0300

    feat: setup the server for production by serving the frontend files as static assets
    - It returns the vite-built index.html file on requests to any endpoint at the root other than "/api/products"
    - Upgrade express to v4.8.0 because `res.sendFile()` is not supported in versions below

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,16 @@ const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies in req.body
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve(); // get the path to the root
+
+  app.use(express.static(path.join(__dirname, "/frontend/dist"))); // serve all contents of frontend/ as a static assets stored in dist/ folder
+  app.get("*", (req, res, next) => {
+    // any endpoint visited at this domain (localhost:5000 or custom domain) apart from /api/products will serve the html file hence the react app
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 connectDB();
 app.listen(PORT, () => {
