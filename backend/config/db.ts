7e1bb7a15342c161d2fcce9cb7b3345056218fa0
Rinidh <rinidhdhokia@gmail.com>
2026-01-10 09:31:13 +0300

    chore(migrate to TS): migrate mongodb connection file db.js to db.ts

---
+++ backend/config/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { logger } from "../logger.js";
+
+export const connectDB = async (): Promise<void> => {
+  const mongoURI = process.env.MONGO_URI;
+
+  try {
+    const conn = await mongoose.connect(mongoURI);
+    logger.info(`Connected to MongoDB : ${conn.connection.host}`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(`Error connecting to MongoDB: ${error.message}`, {
+        stack: error.stack,
+      });
+    } else {
+      logger.error("Unknown error occurred while connecting to MongoDB");
+    }
+
+    process.exit(1);
+  }
+};
