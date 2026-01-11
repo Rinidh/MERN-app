a54362af3379cb60125c25993d6deb962e5e9179
Rinidh <rinidhdhokia@gmail.com>
2026-01-11 14:48:41 +0300

    fix: initialize environment variables before any module accesses them
    Problem: in ESM imports, all imported modules in server.js eg logger.js are run first before the file's own code (which includes `dotenv.config()`). Hence the env vars were undefined and logger.js was crashing the app
    Env var initialization was moved into a separate module `env.ts` that runs at top of server.js

---
+++ backend/config/env.ts
@@ -0,0 +1,4 @@
+// loads envirnment variables before other modules eg logger.ts or db.ts access it
+
+import dotenv from "dotenv";
+dotenv.config();
--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
+import "./config/env.js";
+
 import express, { Response, Request, NextFunction } from "express";
-import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -7,7 +8,6 @@ import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 import { logger } from "./logger.js";
 
-dotenv.config();
 const PORT = Number(process.env.PORT) || 5000;
 
 const app = express();
