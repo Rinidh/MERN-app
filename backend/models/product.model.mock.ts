1390dccbf38c2e3bab66544fb814764e1cddb418
practice1one1 <practice1one1@gmail.com>
2026-01-21 08:30:09 +0000

    test: suppress ts errors in product.model.mocks.ts about type mismatch
    - Mismatch is between `ProductMock.findByIdAndUpdate` and `ProductMock.findByIdAndDelete` about type mismatch between types: mongoose `Query` and `{orFail: updateOrFailMock}`
    - Better to infer `any` than reproducing a whole structured type interface that is hard to maintain
    
    - Updated the project structure tree in reminders.txt

--- backend/models/product.model.mock.ts
+++ backend/models/product.model.mock.ts
@@ -13,7 +13,11 @@ const ProductMock = vi.fn().mockImplementation(function () {
   };
 }) as unknown as Model<ProductDocument>; // type casting to allow setting static properties on the mock function
 ProductMock.find = vi.fn();
-ProductMock.findByIdAndUpdate = vi.fn(() => ({ orFail: updateOrFailMock })); // remaining to fix type errors
-ProductMock.findByIdAndDelete = vi.fn(() => ({ orFail: deleteOrFailMock })); // remaining to fix type errors
+ProductMock.findByIdAndUpdate = vi.fn(
+  () => ({ orFail: updateOrFailMock } as any), // Mongoose Query types are intentionally erased in unit tests and `any` is inferred here intead of mongoose `<Query>`
+);
+ProductMock.findByIdAndDelete = vi.fn(
+  () => ({ orFail: deleteOrFailMock } as any), // Mongoose Query types are intentionally erased in unit tests and `any` is inferred here intead of mongoose `<Query>`
+);
 
 export { ProductMock, saveMock, updateOrFailMock, deleteOrFailMock };
