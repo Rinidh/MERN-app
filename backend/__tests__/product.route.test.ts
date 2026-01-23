274b3be677777b516b3f4af53d7764b40597110b
practice1one1 <practice1one1@gmail.com>
2026-01-23 09:26:57 +0000

    test(routes): add unit test for POST /api/products; reset mocks and express app state before each test

--- backend/__tests__/product.route.test.ts
+++ backend/__tests__/product.route.test.ts
@@ -1,22 +1,32 @@
-import { describe, it, vi, expect } from "vitest";
+import { describe, it, vi, expect, beforeEach } from "vitest";
 import express from "express";
+import type { Express } from "express";
 import request from "supertest";
 import router from "../routes/product.route.js";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  createProduct,
+  getAllProducts,
+} from "../controllers/product.controller.js";
 
 vi.mock("../controllers/product.controller.js", () => ({
   getAllProducts: vi.fn((req, res) => {
     res.status(200).json({ message: "getAllProducts was called" });
   }),
-  createProduct: vi.fn(),
+  createProduct: vi.fn((req, res) => {
+    res.status(201).json({ message: "createProduct was called" });
+  }),
   updateProduct: vi.fn(),
   deleteProduct: vi.fn(),
 }));
 
 describe("product routes", () => {
-  const app = express();
-  // app.use(express.json()); // not needed as supertest already configures this
-  app.use("/api/products", router);
+  let app: Express;
+
+  beforeEach(() => {
+    app = express(); // new app before each test to clear express internal state eg if a test may pollute the req, res objects
+    app.use("/api/products", router);
+    vi.clearAllMocks();
+  });
 
   it("GET /api/products calls getAllProducts", async () => {
     const response = await request(app).get("/api/products");
@@ -25,6 +35,18 @@ describe("product routes", () => {
     expect(response.status).toEqual(200);
     expect(response.body).toEqual(
       expect.objectContaining({ message: "getAllProducts was called" }),
+    );
+  });
+
+  it("POST /api/products calls createProduct", async () => {
+    const response = await request(app)
+      .post("/api/products")
+      .send({ name: "valid name", price: 10, image: "valid image url" });
+
+    expect(createProduct).toHaveBeenCalledOnce();
+    expect(response.status).toEqual(201);
+    expect(response.body).toEqual(
+      expect.objectContaining({ message: "createProduct was called" }),
     );
   });
 });
