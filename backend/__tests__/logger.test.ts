3006cd00d9b01c2b9970733805e148d2333f6b40
Rinidh <rinidhdhokia@gmail.com>
2026-01-25 12:38:32 +0300

    test(log): add unit test for creation of winston logger

---
+++ backend/__tests__/logger.test.ts
@@ -0,0 +1,30 @@
+import { describe, expect, it, vi } from "vitest";
+
+vi.mock("winston", async () => {
+  const actual = await vi.importActual("winston");
+
+  return {
+    default: {
+      ...actual,
+      createLogger: vi.fn(),
+      transports: {
+        File: vi.fn(),
+        Console: vi.fn(),
+        MongoDB: vi.fn(),
+      },
+    },
+  };
+});
+
+describe("logger", () => {
+  it("creates a logger with expected configurations upon import", async () => {
+    const winston = await import("winston");
+    await import("../logger.js");
+
+    expect(winston.default.createLogger).toHaveBeenCalledOnce();
+
+    const config = vi.mocked(winston.default.createLogger).mock.calls[0][0];
+    expect(config).not.toBeUndefined();
+    expect(config?.level).toBe("info");
+  });
+});
