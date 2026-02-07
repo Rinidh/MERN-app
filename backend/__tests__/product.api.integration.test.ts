e7ef47dac38dfc2433344a225ee9a319a76b1dca
Rinidh <rinidhdhokia@gmail.com>
2026-02-07 15:20:31 +0300

    test: add integration test for GET /api/products endpoint that all available products are fetched

--- backend/__tests__/product.api.integration.test.ts
+++ backend/__tests__/product.api.integration.test.ts
@@ -1,10 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it, afterEach } from "vitest";
 import request from "supertest";
+import mongoose from "mongoose";
 
 import { app } from "../server.js";
 import { connectDB } from "../config/db.js";
-import mongoose from "mongoose";
+import Product from "../models/product.model.js";
 
 let mongoServer: MongoMemoryServer;
 
@@ -24,11 +25,40 @@ describe("GET /api/products - Integration tests", () => {
     await mongoServer.stop();
   });
 
+  afterEach(async () => {
+    await Product.deleteMany({});
+  });
+
   it("returns 200 with empty array when no products exist", async () => {
     const res = await request(app).get("/api/products");
 
     expect(res.status).toBe(200);
     expect(Array.isArray(res.body.data)).toBe(true);
     expect(res.body.data.length).toBe(0);
+  });
+
+  it("returns 200 with array of products when they exist in DB", async () => {
+    await Product.create([
+      {
+        name: "product A",
+        price: 1,
+        image: "image A",
+      },
+      {
+        name: "product B",
+        price: 2,
+        image: "image B",
+      },
+    ]);
+
+    const res = await request(app).get("/api/products");
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("data");
+    expect(res.body.data.length).toBe(2);
+
+    const names = res.body.data.map((p: any) => p.name);
+    expect(names[0]).toBe("product A");
+    expect(names[1]).toBe("product B");
   });
 });
