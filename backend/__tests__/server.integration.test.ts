6469179faf159dd79b9d82cb11b89dadf6c9cdaf
practice1one1 <practice1one1@gmail.com>
2026-02-14 08:32:26 +0000

    test(bootstrap): remove redundant tests that show false positives
    The tests for:
    - connection to db
    - express app starting to listen
    in test env would always pass as `startServer` is no longer being called implicitly in server.ts
    The success case test named "connects to database, app listens to requests and info message logs when NODE_ENV!=test" already covers this

--- backend/__tests__/server.integration.test.ts
+++ backend/__tests__/server.integration.test.ts
@@ -1,6 +1,5 @@
 import { describe, vi, expect, it, afterEach, beforeEach } from "vitest";
 import request from "supertest";
-import express from "express";
 
 vi.mock("../config/db.js", () => ({
   connectDB: vi.fn().mockResolvedValue(null),
@@ -13,29 +12,11 @@ vi.mock("../logger.js", () => ({
 import { connectDB } from "../config/db.js";
 import { logger } from "../logger.js";
 
-beforeEach(() => {
-  // beforeEach describe() test suite
-  process.env.NODE_ENV = "test"; // for readability (node env is also set by Vitest)
-});
-
 describe("Server bootstrap", () => {
   afterEach(() => {
     vi.resetModules();
     vi.restoreAllMocks();
     process.env.NODE_ENV = "test";
-  });
-  it("app does not start listening when NODE_ENV=test", async () => {
-    await import("../server.js");
-
-    expect(connectDB).not.toHaveBeenCalled();
-  });
-
-  it("app does not start listening when NODE_ENV=test", async () => {
-    const listenSpy = vi.spyOn(express.application, "listen");
-
-    await import("../server.js");
-
-    expect(listenSpy).not.toHaveBeenCalled();
   });
 
   it("connects to database, app listens to requests and info message logs when NODE_ENV!=test", async () => {
