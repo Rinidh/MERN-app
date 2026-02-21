29eebaa91c375dd8986afd8fdbc856a4fb047f2d
Rinidh <rinidhdhokia@gmail.com>
2026-02-21 14:12:00 +0300

    refactor(errors): replace if statements with switch case for global error handling logic

--- backend/middleware/error.ts
+++ backend/middleware/error.ts
@@ -20,72 +20,72 @@ export const errorHandler = (
   logger.error((err as any).constructor?.name); // custom error name logged for exact description
   logger.error(err);
 
-  if (
-    err instanceof BadRequestError ||
-    err instanceof ValidationError ||
-    err instanceof NotFoundError
-  ) {
-    res.status(err.statusCode).json({
-      errors: err.serializeErrors(),
-    });
-    return;
-  }
+  switch (true) {
+    case err instanceof BadRequestError ||
+      err instanceof ValidationError ||
+      err instanceof NotFoundError: {
+      res.status(err.statusCode).json({
+        errors: err.serializeErrors(),
+      });
+      return;
+    }
 
-  if (err instanceof DocumentCastError) {
-    logger.warn(`Invalid mongo ID detected: ${err.invalidMongoId}`);
-    res.status(err.statusCode).json({
-      errors: err.serializeErrors(),
-    });
-    return;
-  }
+    case err instanceof DocumentCastError: {
+      logger.warn(`Invalid mongo ID detected: ${err.invalidMongoId}`);
+      res.status(err.statusCode).json({
+        errors: err.serializeErrors(),
+      });
+      return;
+    }
 
-  if (err instanceof mongoose.Error.ValidationError) {
-    res.status(422).json({
-      errors: Object.values(err.errors).map((e) => ({ message: e.message })),
-    });
-    return;
-  }
+    case err instanceof mongoose.Error.ValidationError: {
+      res.status(422).json({
+        errors: Object.values(err.errors).map((e) => ({ message: e.message })),
+      });
+      return;
+    }
 
-  if (err instanceof mongoose.Error.CastError) {
-    res.status(422).json({
-      errors: [{ message: err.message }],
-    });
-    return;
-  }
+    case err instanceof mongoose.Error.CastError: {
+      res.status(422).json({
+        errors: [{ message: err.message }],
+      });
+      return;
+    }
 
-  if (
-    err instanceof MongoNetworkError ||
-    err instanceof MongoServerSelectionError ||
-    err instanceof MongoNetworkTimeoutError
-  ) {
-    res.status(503).json({
-      errors: [
-        {
-          message: "Database unavailabe",
-        },
-      ],
-    });
-    return;
-  }
+    case err instanceof MongoNetworkError ||
+      err instanceof MongoServerSelectionError ||
+      err instanceof MongoNetworkTimeoutError: {
+      res.status(503).json({
+        errors: [
+          {
+            message: "Database unavailabe",
+          },
+        ],
+      });
+      return;
+    }
 
-  if ((err as any).code === "ECONNRESET") {
-    res.status(503).json({
-      errors: [
-        {
-          message: "Service is unavailable",
-        },
-      ],
-    });
-    return;
-  }
+    case (err as any).code === "ECONNRESET": {
+      res.status(503).json({
+        errors: [
+          {
+            message: "Service is unavailable",
+          },
+        ],
+      });
+      return;
+    }
 
-  if (err instanceof Error) {
-    logger.error("An unhandled error was thrown");
-  } else {
-    logger.error("An nnknown non-error was thrown");
-  }
+    case err instanceof Error:
+      logger.error("An unhandled error was thrown");
+      return res.status(500).json({
+        errors: [{ message: "Internal Server Error" }],
+      });
 
-  return res.status(500).json({
-    errors: [{ message: "Internal Server Error" }],
-  });
+    default:
+      logger.error("An nnknown non-error was thrown");
+      return res.status(500).json({
+        errors: [{ message: "Internal Server Error" }],
+      });
+  }
 };
