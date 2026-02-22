68382f20cea99ae8c9181ced03bf2bd986d184b5
practice1one1 <practice1one1@gmail.com>
2026-02-22 07:48:38 +0000

    feat: add catch-all route for requests to non-existent endpoints

--- backend/server.ts
+++ backend/server.ts
@@ -9,6 +9,7 @@ import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 import { logger, initMongoDBLogger } from "./logger.js";
 import { errorHandler } from "./middleware/error.js";
+import { NotFoundError } from "./errors/not-found-error.js";
 
 const PORT = Number(process.env.PORT) || 5000;
 
@@ -16,6 +17,9 @@ export const app = express();
 
 app.use(express.json()); // Middleware to parse JSON bodies in req.body
 app.use("/api/products", productRoutes);
+app.use((req, res, next) => {
+  throw new NotFoundError(`❌ Route ${req.originalUrl} not found!`);
+});
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
