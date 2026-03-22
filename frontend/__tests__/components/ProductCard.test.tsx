441808132f3bd2cc8b138d03f359cd9e5aa760dd
Rinidh <rinidhdhokia@gmail.com>
2026-03-22 21:23:19 +0300

    test(ProductCard): group tests under rendering, card action buttons and modal & update form

--- frontend/__tests__/components/ProductCard.test.tsx
+++ frontend/__tests__/components/ProductCard.test.tsx
@@ -31,143 +31,153 @@ beforeEach(() => {
 });
 
 describe("ProductCard", () => {
-  it("renders a card with image, name heading, price, edit and delete buttons", () => {
-    setup();
+  describe("rendering", () => {
+    it("renders a card with image, name heading, price, edit and delete buttons", () => {
+      setup();
 
-    expect(screen.getByRole("img")).toHaveAttribute("src", product.image);
-    expect(
-      screen.getByRole("heading", { name: product.name }),
-    ).toBeInTheDocument();
-    expect(
-      screen.getByText(new RegExp(String(product.price))),
-    ).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: /edit/i })).toBeInTheDocument();
-    expect(screen.getByRole("button", { name: /delete/i })).toBeInTheDocument();
-  });
-
-  it("does not render modal initially and it opens on clicking edit button", async () => {
-    const { user } = setup();
-
-    expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
-
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    expect(screen.getByRole("dialog")).toBeInTheDocument();
-  });
-
-  it("modal displays inputs with current product name, price and image", async () => {
-    const { user } = setup();
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
-    expect(screen.getByPlaceholderText(/price/i)).toHaveValue(
-      String(product.price),
-    );
-    expect(screen.getByPlaceholderText(/image/i)).toHaveValue(product.image);
-  });
-
-  it("closes modal when cancel button is clicked", async () => {
-    const { user } = setup();
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    const cancelButton = screen.getByRole("button", { name: "Cancel" });
-    await user.click(cancelButton);
-
-    await waitFor(() =>
-      expect(screen.queryByRole("dialog")).not.toBeInTheDocument(),
-    );
-  });
-
-  it("closes modal when close button is clicked", async () => {
-    const { user } = setup();
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    const closeButton = screen.getByRole("button", { name: "Close" });
-    await user.click(closeButton);
-
-    await waitFor(() =>
-      expect(screen.queryByRole("dialog")).not.toBeInTheDocument(),
-    );
-  });
-
-  it("updates inputs values when user types", async () => {
-    const { user } = setup();
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    const nameInput = screen.getByPlaceholderText(/name/i);
-    const priceInput = screen.getByPlaceholderText(/price/i);
-    const imageInput = screen.getByPlaceholderText(/image/i);
-    await user.type(nameInput, "added to name");
-    await user.type(priceInput, ".5");
-    await user.type(imageInput, ".png");
-
-    expect(
-      screen.getByDisplayValue(product.name + "added to name"),
-    ).toBeInTheDocument();
-    expect(screen.getByDisplayValue(product.price + ".5")).toBeInTheDocument();
-    expect(
-      screen.getByDisplayValue(product.image + ".png"),
-    ).toBeInTheDocument();
-  });
-
-  it("calls deleteProduct with product id on clicking delete button", async () => {
-    setup();
-
-    const deleteButton = screen.getByRole("button", { name: /delete/i });
-    await userEvent.click(deleteButton);
-
-    expect(deleteProduct).toHaveBeenCalledOnce();
-    expect(deleteProduct).toHaveBeenCalledWith(product._id);
-  });
-
-  it("update button in modal is disabled until change in field values", async () => {
-    const { user } = setup();
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    expect(screen.getByRole("button", { name: /update/i })).toBeDisabled();
-
-    const nameInput = screen.getByPlaceholderText(/name/i);
-    await user.type(nameInput, "new product name");
-
-    expect(screen.getByRole("button", { name: /update/i })).toBeEnabled();
-  });
-
-  it("keeps update button disabled when only whitespace changes occur", async () => {
-    const { user } = setup();
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    const priceInput = screen.getByPlaceholderText(/price/i);
-    await user.type(priceInput, "   ");
-
-    expect(screen.getByRole("button", { name: /update/i })).toBeDisabled();
-  });
-
-  it("clicking update button in modal calls updateProduct with product id and new values", async () => {
-    const { user } = setup();
-    const editButton = screen.getByRole("button", { name: /edit/i });
-    await user.click(editButton);
-
-    const nameInput = screen.getByPlaceholderText(/name/i);
-    await user.clear(nameInput);
-    await user.type(nameInput, "new product");
-    const updateButton = screen.getByRole("button", { name: /update/i });
-    await user.click(updateButton);
-
-    expect(updateProduct).toHaveBeenCalledOnce();
-    expect(updateProduct).toHaveBeenCalledWith(product._id, {
-      name: "new product",
-      price: "10",
-      image: "img1",
+      expect(screen.getByRole("img")).toHaveAttribute("src", product.image);
+      expect(
+        screen.getByRole("heading", { name: product.name }),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText(new RegExp(String(product.price))),
+      ).toBeInTheDocument();
+      expect(screen.getByRole("button", { name: /edit/i })).toBeInTheDocument();
+      expect(
+        screen.getByRole("button", { name: /delete/i }),
+      ).toBeInTheDocument();
     });
-    await waitFor(() =>
-      expect(screen.queryByRole("dialog")).not.toBeInTheDocument(),
-    );
+  });
+
+  describe("card action buttons", () => {
+    it("does not render modal initially and it opens on clicking edit button", async () => {
+      const { user } = setup();
+
+      expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
+
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      expect(screen.getByRole("dialog")).toBeInTheDocument();
+    });
+
+    it("calls deleteProduct with product id on clicking delete button", async () => {
+      setup();
+
+      const deleteButton = screen.getByRole("button", { name: /delete/i });
+      await userEvent.click(deleteButton);
+
+      expect(deleteProduct).toHaveBeenCalledOnce();
+      expect(deleteProduct).toHaveBeenCalledWith(product._id);
+    });
+  });
+
+  describe("modal and product-update form", () => {
+    it("modal displays inputs with current product name, price and image", async () => {
+      const { user } = setup();
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      expect(screen.getByPlaceholderText(/name/i)).toHaveValue(product.name);
+      expect(screen.getByPlaceholderText(/price/i)).toHaveValue(
+        String(product.price),
+      );
+      expect(screen.getByPlaceholderText(/image/i)).toHaveValue(product.image);
+    });
+
+    it("closes modal when cancel button is clicked", async () => {
+      const { user } = setup();
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      const cancelButton = screen.getByRole("button", { name: "Cancel" });
+      await user.click(cancelButton);
+
+      await waitFor(() =>
+        expect(screen.queryByRole("dialog")).not.toBeInTheDocument(),
+      );
+    });
+
+    it("closes modal when close button is clicked", async () => {
+      const { user } = setup();
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      const closeButton = screen.getByRole("button", { name: "Close" });
+      await user.click(closeButton);
+
+      await waitFor(() =>
+        expect(screen.queryByRole("dialog")).not.toBeInTheDocument(),
+      );
+    });
+
+    it("updates inputs values when user types", async () => {
+      const { user } = setup();
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      const nameInput = screen.getByPlaceholderText(/name/i);
+      const priceInput = screen.getByPlaceholderText(/price/i);
+      const imageInput = screen.getByPlaceholderText(/image/i);
+      await user.type(nameInput, "added to name");
+      await user.type(priceInput, ".5");
+      await user.type(imageInput, ".png");
+
+      expect(
+        screen.getByDisplayValue(product.name + "added to name"),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByDisplayValue(product.price + ".5"),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByDisplayValue(product.image + ".png"),
+      ).toBeInTheDocument();
+    });
+
+    it("update button in modal is disabled until change in field values", async () => {
+      const { user } = setup();
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      expect(screen.getByRole("button", { name: /update/i })).toBeDisabled();
+
+      const nameInput = screen.getByPlaceholderText(/name/i);
+      await user.type(nameInput, "new product name");
+
+      expect(screen.getByRole("button", { name: /update/i })).toBeEnabled();
+    });
+
+    it("keeps update button disabled when only whitespace changes occur", async () => {
+      const { user } = setup();
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      const priceInput = screen.getByPlaceholderText(/price/i);
+      await user.type(priceInput, "   ");
+
+      expect(screen.getByRole("button", { name: /update/i })).toBeDisabled();
+    });
+
+    it("clicking update button in modal calls updateProduct with product id and new values", async () => {
+      const { user } = setup();
+      const editButton = screen.getByRole("button", { name: /edit/i });
+      await user.click(editButton);
+
+      const nameInput = screen.getByPlaceholderText(/name/i);
+      await user.clear(nameInput);
+      await user.type(nameInput, "new product");
+      const updateButton = screen.getByRole("button", { name: /update/i });
+      await user.click(updateButton);
+
+      expect(updateProduct).toHaveBeenCalledOnce();
+      expect(updateProduct).toHaveBeenCalledWith(product._id, {
+        name: "new product",
+        price: "10",
+        image: "img1",
+      });
+      await waitFor(() =>
+        expect(screen.queryByRole("dialog")).not.toBeInTheDocument(),
+      );
+    });
   });
 });
