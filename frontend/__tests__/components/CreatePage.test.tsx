3cc852238bc8b9b2302677379f49f2aadc4db846
Rinidh <rinidhdhokia@gmail.com>
2026-04-11 12:47:28 +0300

    test(CreatePage): add tests for updating field values as user types

--- frontend/__tests__/components/CreatePage.test.tsx
+++ frontend/__tests__/components/CreatePage.test.tsx
@@ -1,8 +1,11 @@
 import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { CreatePage } from "../../src/components/CreatePage";
+import userEvent from "@testing-library/user-event";
 
 describe("CreatePage", () => {
+  const user = userEvent.setup({ delay: null });
+
   it("renders a page heading, empty inputs with placeholders for name, price and image, and an Add button", () => {
     render(<CreatePage />);
 
@@ -14,9 +17,23 @@ describe("CreatePage", () => {
     expect(screen.getByPlaceholderText(/image/i)).toHaveValue("");
     expect(screen.getByRole("button", { name: /add/i })).toBeInTheDocument();
   });
+
+  it("typing into inputs updates field values", async () => {
+    render(<CreatePage />);
+
+    const nameInput = screen.getByPlaceholderText(/name/i);
+    const priceInput = screen.getByPlaceholderText(/price/i);
+    const imageInput = screen.getByPlaceholderText(/image/i);
+    await user.type(nameInput, "product name");
+    await user.type(priceInput, "10");
+    await user.type(imageInput, "img.png");
+
+    expect(nameInput).toHaveValue("product name");
+    expect(priceInput).toHaveValue(10);
+    expect(imageInput).toHaveValue("img.png");
+  });
 });
 
-// renders a page heading, empty inputs with placeholders for name, price and image, and an Add button
 // typing into inputs updates field values
 // clicking the Add button calls createProduct with the values input in the fields
 // button shows a spinner when in loading state
