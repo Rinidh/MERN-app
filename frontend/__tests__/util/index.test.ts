7660403a8f7705e0372a313a0241aadd2d3307d0
Rinidh <rinidhdhokia@gmail.com>
2026-04-26 13:51:51 +0300

    test(util): add tests for safeParseJson for propagating Json parse error when missed to handle within

--- frontend/__tests__/util/index.test.ts
+++ frontend/__tests__/util/index.test.ts
@@ -1,6 +1,12 @@
 import { it, expect, describe } from "vitest";
 import { safeParseJson } from "../../src/util";
 
+const mockResponse = (options: { ok: boolean; json?: () => Promise<any> }) =>
+  ({
+    ok: options.ok,
+    json: options.json || vi.fn().mockRejectedValue(new Error("failed")),
+  }) as unknown as Response;
+
 describe("safeParseJson", () => {
   it("returns parsed JSON when response status is ok", async () => {
     const responseData = {
@@ -9,14 +15,23 @@ describe("safeParseJson", () => {
       image: "img.png",
     };
 
-    const res = {
+    const res = mockResponse({
       ok: true,
       json: vi.fn().mockResolvedValue(responseData),
-    } as unknown as Response;
+    });
 
     const data = await safeParseJson<typeof responseData>(res);
     // if safeParseJson throws, it'll be an automatic test failure
 
     expect(data).toEqual(responseData);
+  });
+
+  it("propagate JSON parsing error when ok=true but fails to parse json", async () => {
+    const res = mockResponse({
+      ok: true,
+      json: vi.fn().mockRejectedValue("Failed to parse JSON"),
+    });
+
+    await expect(safeParseJson(res)).rejects.toThrow("Failed to parse JSON");
   });
 });
